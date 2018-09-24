d9914cd97cc869f68f163c971c6ec8333374fcbb
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-24 15:47:10 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=1;
+      	int b=2;
+       	int c=a+b;
+      	int d=a-b;
+  		int e=a*b;
+		int f=b/a;
+		int g=b%a;
+		int h=b++;
+		int i=b—;
+     	int j=a&b;
+		int k=a|b;
+		int l=a^b;
+ 		int m=~a;
+		int n=a<<2;
+		int o=a>>2;
+		int p=a>>>2;
+		int q=a&&b;
+		int r=a||b;
+		int s=!(a&&b);
+      System.out.print("\n" +c+ "\n" +d+ "\n" +e+ "\n" +f+ "\n" +g+ "\n" +h+ "\n" +i+ "\n" +j+ "\n" +k+ "\n" +l+ "\n" +m+ "\n" +n+ "\n" +o+ "\n" +p+ "\n" +q+ "\n" +r+ "\n" +s+) ;
+	}
