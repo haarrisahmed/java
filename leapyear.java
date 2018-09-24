5987fb1ce3d56d2b45b608276f8609558dff4dcf
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-24 15:43:26 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+class leapYear {
+public static void main (String args[]){
+    int leapYear = 2001; // initialize leapYear variable to 2001
+
+    if (leapYear <= 2100){
+        for (int x = 0; x <= 10; x++){ // print ten leap years per line
+            System.out.print(leapYear + " ");
+            leapYear = leapYear + 4;
+        }// end of for loop
+        System.out.println(" ");
+        int x = 0;
+    } // end of if statement
+
+}
+	}
+}
