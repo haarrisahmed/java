db2fece21907bfdd22147ea5bf586fed9395d131
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-24 15:46:32 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,12 @@
+public static void main (String[] args) throws java.lang.Exception
+	{
+		String name = "Sridevi";
+      	String city = "Chennai";
+      	int age = 18;
+      	float height = 5.3f;
+        boolean married=true; 
+      	String sex="F";
+       	double citypopulation=4.828.853;
+      System.out.println("\n name:" +name+"\n City:"+city+ "\n age:"+age+ "\n height:"+height+ "\n married:"+married "\n Sex:"+sex+ "\n Citypopulation:" +citypopulation);
+	}
+}
