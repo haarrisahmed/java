d417dca4ebea5ab8069c1bf1036fa5a829eae4e9
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-24 15:48:16 +0530

    Create  If Else ladder.java

---
+++ If Else ladder.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a=100000;
+      if(a>20000)
+      {
+         	System.out.println("Free Trolley Suitcase");
+	}
+      else if(a>10000)
+      {
+        	System.out.println("Free Travelbag");
+      }
+      else if(a>5000)
+      {
+        	System.out.println("Free BackPck");
+      }
+      else if(a<5000)
+      {
+        	System.out.println("Collect money even for cover we give");
+      }
+    }
+}
