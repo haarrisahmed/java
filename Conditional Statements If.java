b074aea2a30421d447dbc6fef730378228908190
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-24 15:47:42 +0530

    Create Conditional Statements If.java

---
+++ Conditional Statements If.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=60;
+      if (a<50)
+      {
+        System.out.print("box office hit")
+      }
+      else
+      {
+        system.out.print("flop")
+      }
+    }
+	}
+}
