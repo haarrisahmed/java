5a4c4758c6d4b389f04833cbf51fa9f5e13f0f45
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-25 14:05:11 +0530

    Create Given an array of integers, every element appears twice except for one.java

---
+++ Given an array of integers, every element appears twice except for one.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int findSingle(int ar[], int ar_size)
+    {
+        // Do XOR of all elements and return
+        int res = ar[0];
+        for (int i = 1; i < ar_size; i++)
+            res = res ^ ar[i];
+ 
+        return res;
+    }
+ 
+// Driver code
+int main()
+    {
+        int ar[] = {2, 3, 5, 4, 5, 3, 4};
+        int n = sizeof(ar) / sizeof(ar[0]);
+        cout << "Element occurring once is "
+             << findSingle(ar, n);
+        return 0;
+    }
+	}
+}
