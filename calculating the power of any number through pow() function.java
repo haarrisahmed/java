b0652d04c523c0d9f400485745c10b0e7406ecf7
haarrisahmed <42799616+haarrisahmed@users.noreply.github.com>
2018-09-25 14:21:48 +0530

    Create calculating the power of any number through pow() function.java

---
+++ calculating the power of any number through pow() function.java
@@ -0,0 +1,10 @@
+public class ExampleProgram {
+
+ public static void main(String[] args) {
+
+  int basenumber = 2, exponent = 3;
+  double pow = Math.pow(basenumber, exponent);
+
+  System.out.println("Result: " + pow);
+ }
+}
